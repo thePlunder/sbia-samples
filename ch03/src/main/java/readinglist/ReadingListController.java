402eaa667868417676811d590eb85387403dd2e7
Plunder <theplunder@gmail.com>
2018-01-25 21:23:15 +0100

    formatting

--- ch03/src/main/java/readinglist/ReadingListController.java
+++ ch03/src/main/java/readinglist/ReadingListController.java
@@ -15,46 +15,51 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @Controller
 @RequestMapping("/")
 @ConfigurationProperties("amazon")
-public class ReadingListController {
+public class ReadingListController
+{
 
 	private ReadingListRepository readingListRepository;
-  private AmazonProperties amazonConfig;
+	private AmazonProperties amazonConfig;
 
 	@Autowired
-	public ReadingListController(ReadingListRepository readingListRepository,
-	    AmazonProperties amazonConfig) {
+	public ReadingListController(ReadingListRepository readingListRepository, AmazonProperties amazonConfig)
+	{
 		this.readingListRepository = readingListRepository;
-    this.amazonConfig = amazonConfig;
+		this.amazonConfig = amazonConfig;
 	}
-	
-	@RequestMapping(method=RequestMethod.GET, value="/fail")
-	public void fail() {
-	  throw new RuntimeException();
+
+	@RequestMapping(method = RequestMethod.GET, value = "/fail")
+	public void fail()
+	{
+		throw new RuntimeException();
 	}
-	
-	@ExceptionHandler(value=RuntimeException.class)
-	@ResponseStatus(value=HttpStatus.BANDWIDTH_LIMIT_EXCEEDED)
-	public String error() {
-	  return "error";
+
+	@ExceptionHandler(value = RuntimeException.class)
+	@ResponseStatus(value = HttpStatus.BANDWIDTH_LIMIT_EXCEEDED)
+	public String error()
+	{
+		return "error";
 	}
-	
-	
-	@RequestMapping(method=RequestMethod.GET)
-	public String readersBooks(Reader reader, Model model) {
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String readersBooks(Reader reader, Model model)
+	{
 		List<Book> readingList = readingListRepository.findByReader(reader);
-		if (readingList != null) {
+		if (readingList != null)
+		{
 			model.addAttribute("books", readingList);
 			model.addAttribute("reader", reader);
 			model.addAttribute("amazonID", amazonConfig.getAssociateId());
 		}
 		return "readingList";
 	}
-	
-	@RequestMapping(method=RequestMethod.POST)
-	public String addToReadingList(Reader reader, Book book) {
+
+	@RequestMapping(method = RequestMethod.POST)
+	public String addToReadingList(Reader reader, Book book)
+	{
 		book.setReader(reader);
 		readingListRepository.save(book);
 		return "redirect:/";
 	}
-	
+
 }
